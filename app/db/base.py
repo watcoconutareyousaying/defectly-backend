ef22fb906c978ed734f0193d6d42c5cf60a174a3
Nantha <nandhafsd6@gmail.com>
2025-09-04 10:18:55 +0530

    add missing migrations files for plans

--- app/db/base.py
+++ app/db/base.py
@@ -9,3 +9,4 @@ from app.models.otp import OTP
 from app.models.tokens import Token
 from app.models.activity_log import ActivityLog
 from app.models.project import Project
+from app.models.plan import Plan
